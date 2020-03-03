510fe89f5c54594e0195ab76884dc356ac452a8d
Challa <spandana.challa@accenture.com>
2020-03-03 14:22:27 +0530

    'EventBinding'

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,16 +8,23 @@ export class ServerComponent implements OnInit{
     serverId:number = 10;
     servername:string = 'Explorer';
     allowNewServer = true;
+    ServerStatus = "no server was created"
     serverDetails(){
          return this.servername;
+    }
+    OnCreateServer(){
+        console.log("Hiii");
+       this.ServerStatus = "Server created"
     }
     constructor(){
         setTimeout(()=>{    //<<<---    using ()=> syntax
             this.allowNewServer = false;
        }, 3000);
 }
+
 ngOnInit()
 {
 
 }
+
 }
